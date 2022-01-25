102bd23a4a506276813580a24e7395a8ff109400
Miki Blasco <blascom@7@gmail.com>
2022-01-25 20:01:41 +0100

    1

--- js/index.js
+++ js/index.js
@@ -21,13 +21,14 @@ function calculateAll() {
 }
 
 
-
-
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+  const toRemove= target.parentNode.parentNode
+  toRemove.remove()
+  calculateAll()
 }
 
 // ITERATION 5
@@ -39,6 +40,9 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+
+  const removeBtn = document.querySelectorAll('.btn-remove');
+  removeBtn.forEach(element => element.addEventListener('click', removeProduct))
 
   //... your code goes here
 });
